733188f0b85638df1aaa42a34856a22affe78239
xiaowu <2579505894@qq.com>
2016-08-17 22:52:03 +0800

    修改之前GridLayoutManager item高度的bug

--- app/src/main/java/com/example/maxiaowu/simpleimageloader/MainActivity.java
+++ app/src/main/java/com/example/maxiaowu/simpleimageloader/MainActivity.java
@@ -1,35 +1,58 @@
 package com.example.maxiaowu.simpleimageloader;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-    RecyclerView recyclerView;
-    ArrayList<String> dataset=new ArrayList<>(Arrays.asList("http://10.117.4.49:8080/dessert.png",
-            "http://10.117.4.49:8080/tecent.png",
-            "http://10.117.4.49:8080/channel.png",
-            "http://10.117.4.49:8080/Hydrangeas.png",
-            "http://10.117.4.49:8080/kaola.png",
-            "http://10.117.4.49:8080/flower.png",
-            "http://10.117.4.49:8080/tomcat.png",
-            "http://10.117.4.49:8080/1.png",
-            "http://10.117.4.49:8080/dessert.png","http://10.117.4.49:8080/dessert.png","http://10.117.4.49:8080/dessert.png",
-            "http://10.117.4.49:8080/dessert.png","http://10.117.4.49:8080/dessert.png","http://10.117.4.49:8080/dessert.png",
-            "http://10.117.4.49:8080/dessert.png",
-            "http://10.117.4.49:8080/dessert.png","http://10.117.4.49:8080/dessert.png","http://10.117.4.49:8080/dessert.png",
-            "http://10.117.4.49:8080/dessert.png","http://10.117.4.49:8080/dessert.png"));
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+//    ArrayList<String> dataset=new ArrayList<>(Arrays.asList("http://192.168.1.106:8080/dessert.jpeg",
+//            "http://192.168.1.106:8080/tecent.jpeg",
+//            "http://192.168.1.106:8080/channel.jpeg",
+//            "http://192.168.1.106:8080/Hydrangeas.jpeg",
+//            "http://192.168.1.106:8080/kaola.jpeg",
+//            "http://192.168.1.106:8080/flower.jpeg",
+//            "http://192.168.1.106:8080/tomcat.jpeg",
+//            "http://192.168.1.106:8080/1.jpeg",
+//            "http://192.168.1.106:8080/dessert.jpeg","http://192.168.1.106:8080/dessert.jpeg","http://192.168.1.106:8080/dessert.jpeg",
+//            "http://192.168.1.106:8080/dessert.jpeg","http://192.168.1.106:8080/dessert.jpeg","http://192.168.1.106:8080/dessert.jpeg",
+//            "http://192.168.1.106:8080/dessert.jpeg",
+//            "http://192.168.1.106:8080/dessert.jpeg","http://192.168.1.106:8080/dessert.jpeg","http://192.168.1.106:8080/dessert.jpeg",
+//            "http://192.168.1.106:8080/dessert.jpeg","http://192.168.1.106:8080/dessert.jpeg"));    
+    ArrayList<String> dataset=new ArrayList<>(Arrays.asList("http://192.168.1.106:8080/dessert.jpeg",
+            "http://192.168.1.106:8080/tecent.jpeg",
+            "http://192.168.1.106:8080/channel.jpeg",
+            "http://192.168.1.106:8080/Hydrangeas.jpeg",
+            "http://192.168.1.106:8080/kaola.jpeg",
+            "http://192.168.1.106:8080/flower.jpeg",
+            "http://192.168.1.106:8080/tomcat.jpeg",
+            "http://192.168.1.106:8080/1.jpeg",
+            "http://192.168.1.106:8080/dessert.jpeg",
+        "http://192.168.1.106:8080/tecent.jpeg",
+        "http://192.168.1.106:8080/dessert.jpeg",
+        "http://192.168.1.106:8080/tecent.jpeg",
+        "http://192.168.1.106:8080/dessert.jpeg",
+        "http://192.168.1.106:8080/tecent.jpeg",
+            "http://192.168.1.106:8080/dessert.jpeg",
+        "http://192.168.1.106:8080/tecent.jpeg",
+        "http://192.168.1.106:8080/dessert.jpeg",
+        "http://192.168.1.106:8080/tecent.jpeg",
+            "http://192.168.1.106:8080/dessert.jpeg",
+        "http://192.168.1.106:8080/dessert.jpeg"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView= (RecyclerView) findViewById(R.id.rv_image_wall);
-        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+//        recyclerView.setLayoutManager(new GridLayoutManager(this,3));
+        recyclerView.setLayoutManager(linearLayoutManager=new LinearLayoutManager(this));
+        linearLayoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setAdapter(new ImageAdapter(this,dataset));
     }
 }
